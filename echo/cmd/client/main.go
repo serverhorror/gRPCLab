83422047ae5d55a410505b0415dd317a41264fda
Martin Marcher <martin@marcher.name>
2016-10-04 10:37:05 +0200

    Initial commit

---
+++ echo/cmd/client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/serverhorror/go-playground/gRPCLab/echo"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+// type EchoClient struct{}
+
+// func Echo(ctx context.Context, in *pb.Request, opts ...grpc.CallOption) (*pb.Response, error) {
+// 	return nil, nil
+// }
+
+func main() {
+	// client := EchoClient{}
+
+	var opts []grpc.DialOption
+	opts = append(opts,
+		grpc.WithInsecure(),
+		grpc.WithUserAgent("gRPCLab echo"),
+	)
+	conn, err := grpc.Dial("[::1]:8000", opts...)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewEchoClient(conn)
+	request := new(pb.Request)
+	request.Message = "ping"
+
+	resp, err := client.Echo(context.Background(), request)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Resp: %v", resp)
+}
