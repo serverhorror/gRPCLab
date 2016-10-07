0817ce2bc5bb475322e25b3c177bc09c4d214fcd
Martin Marcher <martin@marcher.name>
2016-10-07 13:15:22 +0200

    Use TLS security

--- echo/cmd/client/main.go
+++ echo/cmd/client/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	pb "github.com/serverhorror/go-playground/gRPCLab/echo"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+)
+
+var (
+	caFile = flag.String("caFile", "", "Path to CA")
 )
 
 // type EchoClient struct{}
@@ -15,15 +22,30 @@ import (
 // 	return nil, nil
 // }
 
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	log.SetPrefix("gRPCLab::echo::client ")
+}
+
 func main() {
+	log.Print("Started and Initialized")
 	// client := EchoClient{}
 
+	flag.Parse()
+
 	var opts []grpc.DialOption
+	var sn string
+
+	creds, err := credentials.NewClientTLSFromFile(*caFile, sn)
+	if err != nil {
+		grpclog.Fatalf("Failed to create TLS credentials %v", err)
+	}
+
 	opts = append(opts,
-		grpc.WithInsecure(),
+		grpc.WithTransportCredentials(creds),
 		grpc.WithUserAgent("gRPCLab echo"),
 	)
-	conn, err := grpc.Dial("[::1]:8000", opts...)
+	conn, err := grpc.Dial("localhost:8000", opts...)
 	if err != nil {
 		panic(err)
 	}
@@ -33,10 +55,12 @@ func main() {
 	request := new(pb.Request)
 	request.Message = "ping"
 
+	log.Printf("Sending %v", request)
 	resp, err := client.Echo(context.Background(), request)
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("Response: %v", resp)
+	log.Print("Done Sending")
 
-	log.Printf("Resp: %v", resp)
 }
--- echo/cmd/server/main.go
+++ echo/cmd/server/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
 
 	pb "github.com/serverhorror/go-playground/gRPCLab/echo"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
 )
 
-type EchoServer struct{}
+var (
+	certFile = flag.String("certFile", "", "Server Certificate to use")
+	keyFile  = flag.String("keyFile", "", "Server Key to use")
+)
+
+type EchoServer struct {
+	keyFile  string
+	certFile string
+}
+
+func NewEchoServer(keyFile string, certFile string) (*EchoServer, error) {
+	e := &EchoServer{
+		keyFile:  keyFile,
+		certFile: certFile,
+	}
+	return e, nil
+}
 
 func (e EchoServer) Echo(ctx context.Context, in *pb.Request) (*pb.Response, error) {
 	log.Printf("Received a message(%T): %v", in, in)
@@ -25,16 +44,28 @@ func (e EchoServer) Echo(ctx context.Context, in *pb.Request) (*pb.Response, err
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
-	log.SetPrefix("gRPCLab::echo ")
+	log.SetPrefix("gRPCLab::echo::server ")
 }
 
 func main() {
-	srv := EchoServer{}
+
+	flag.Parse()
+
+	srv, err := NewEchoServer(*certFile, *keyFile)
+	if err != nil {
+		panic(err)
+	}
 	listener, err := net.Listen("tcp", "[::1]:8000")
 	if err != nil {
 		panic(err)
 	}
 	var opts []grpc.ServerOption
+	creds, err := credentials.NewServerTLSFromFile(srv.certFile, srv.keyFile)
+	if err != nil {
+		grpclog.Fatalf("Failed to generate credentials %v", err)
+	}
+	opts = []grpc.ServerOption{grpc.Creds(creds)}
+
 	grpcServer := grpc.NewServer(opts...)
 	pb.RegisterEchoServer(grpcServer, srv)
 	grpcServer.Serve(listener)
